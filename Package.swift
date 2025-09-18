101cc24f3cda91250f1f335338c19316add8b43e
alex_stergiou <alex.stergiou@hotmail.com>
2025-09-18 12:06:37 +0100

    renamed package

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "Tapp-Networking",
+    name: "TappNetworking",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Tapp-Networking",
-            targets: ["Tapp-Networking"]),
+            name: "TappNetworking",
+            targets: ["TappNetworking"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Tapp-Networking"),
+            name: "TappNetworking"),
         .testTarget(
             name: "Tapp-NetworkingTests",
-            dependencies: ["Tapp-Networking"]
+            dependencies: ["TappNetworking"]
         ),
     ]
 )
